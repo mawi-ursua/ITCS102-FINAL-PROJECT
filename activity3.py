178694b2e4350d29c9b6fd0afaed13bc2f39a802
Maureen Jane Ursua <mawinjeynursua@gmail.com>
2024-12-08 21:32:23 +0800

    Create activity3.py

---
+++ activity3.py
@@ -0,0 +1,40 @@
+fullname = input("Please input your Full name: ")
+age = input("Please input your Age: ")
+birthdate = input("Please input your Birthday: ")
+birthplace = input("Please input your Birthplace: ")
+nationality = input("Please input your Nationality: ")
+gender = input("Please input your Gender: ")
+province = input("Please input your Province: ")
+city = input("Please input your City: ")
+barangay = input("Please input your Barangay: ")
+civilstatus = input("Please input your Civil status: ")
+religion = input("Please input your Religion: ")
+emailaddress = input("Please input your emailaddress: ")
+phonenumber = input("Please input your Phone number: ")
+skills = input("Please input your skills: ")
+talent = input("Please input your talents: ")
+fathersfullname = input("Please input your Father's Full name: ")
+fathersage = input("Please input your Father's age: " )
+fathersoccupation = input("Please input your Father's occupation: ")
+mothersfullname = input("Please input your Mother's Full name: ")
+mothersage = input("Please input your Mother's age:" )
+mothersoccupation = input("Please input your Mother's occupation: ")
+
+print("\nBiodata"
+"\n\nPersonal Information:"
+"\n\nHi, my name is " + fullname + 
+"\nand I am " + age + " years old."
+"\nMy birthday is on " + birthdate + 
+",\nand my birthplace is in " + birthplace +
+".\nI am a " + nationality +
+",\nand my gender is " + gender + 
+".\nI live in the province of " + province + ", in the city of " + city + ", at the barangay " + barangay +  
+".\nMy civil status is " + civilstatus + 
+".\nMy religion is " + religion +
+".\nMy skills are " + skills +
+", and\nMy talents are " + talent +
+".\nYou can contact me at my email, " + emailaddress + " and my phone number, " + phonenumber +
+".\n\nParents Information:\n\nMy Father's name is " + fathersfullname + 
+".\nHe is " + fathersage + "years old.\nHis occupation is " + fathersoccupation + 
+", and\nMy Mother's name is " + mothersfullname +
+".\nShe is " + mothersage + " years old.\nHer occupation is " + mothersoccupation + ".")
