bcaa0342b72d8ebd887c3ff30fc26e16950c4a85
Maureen Jane Ursua <mawinjeynursua@gmail.com>
2024-12-16 00:36:58 +0800

    Create code_challenge4.py

---
+++ code_challenge4.py
@@ -0,0 +1,18 @@
+number1 = eval(input("enter a number --> "))
+number2 = eval(input("enter a number --> "))
+
+answer1 = number1 + number2 
+answer2 = number1 - number2 
+answer3 = number1 * number2 
+answer4 = number1 / number2 
+answer5 = number1 % number2 
+answer6 = number1 ** number2 
+answer7 = number1 // number2 
+
+print("\nThe sum of", number1 , "and", number2 , "is" , answer1 ,)
+print("The difference of" , number1 , "and" , number2 , "is" , answer2 ,)
+print("The product of" , number1 , "and" , number2 , "is" , answer3 ,)
+print("The quotient of" , number1 , "and" , number2 , "is" , answer4 ,)
+print("The modulo of" , number1 , "and" , number2 , "is" , answer5 ,)
+print("The exponent of" , number1 , "and" , number2 , "is" , answer6 ,)
+print("The floordivision of" , number1 , "and" , number2 , "is" , answer7 ,)
