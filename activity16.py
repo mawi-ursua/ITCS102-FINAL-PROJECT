13e6c578f310260c31efb93b6a4bd2c5956cf4a6
Maureen Jane Ursua <mawinjeynursua@gmail.com>
2024-12-17 21:37:40 +0800

    Update activity16.py

--- activity16.py
+++ activity16.py
@@ -1,6 +1,13 @@
-for x in range(1,16):
-    for z in range(1, x+1):
-        print(" ", end= " ")
-    for y in range( 16, x, -1):
-        print("*", end=" ")
-    print()
+for x in range(1, 9, 2):
+    for y in range(7, x-1, -1):
+        print(" ", end=" ")
+    for z in range(x, 0, -1):
+        print(" * ", end=" ")  
+    print()    
+
+for x in range(1, 9, 2):
+    for y in range(x+2):
+        print(" ", end=" ")
+    for z in range(6, x, -1):
+        print(" * ", end=" ")  
+    print()  
